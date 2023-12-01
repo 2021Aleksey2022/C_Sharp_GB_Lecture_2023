8caaa9de83dc9f8706518ec2ed21d7317ab362b7
Алескей Щербаков <manay.ru@mail.ru>
2023-12-01 19:41:49 +1000

    Тип переменной double

---
+++ Lecture_1/Example004_TypeDouble/TypeDouble.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_1.Example004_TypeDouble
+{
+    class TypeDouble
+    {
+        static void Main(string[] args)
+        {
+            double numberA = 12;
+            double numberB = 5;
+            double result = numberA / numberB;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
