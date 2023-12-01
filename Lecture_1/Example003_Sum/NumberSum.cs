dda0a05fdc260ee7304e5ec6b5665d831816e222
Алескей Щербаков <manay.ru@mail.ru>
2023-12-01 19:38:32 +1000

    Программа выводит сумму чисел

---
+++ Lecture_1/Example003_Sum/NumberSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_1.Example003_Sum
+{
+    class NumberSum
+    {
+        static void Main(string[] args)
+        {
+            int numberA = new Random().Next(1, 10);
+            Console.WriteLine($"Первое число: {numberA}");
+            int numberB = new Random().Next(0, 20);
+            Console.WriteLine($"Второе число: {numberB}");
+            int result = numberA + numberB;
+            Console.WriteLine($"Сумма чисел: {result}");
+            Console.ReadLine();
+        }
+    }
+}
