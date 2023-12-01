2fe20ce74bd63fc2025f4240b6f7aa5d841c2bc5
Алескей Щербаков <manay.ru@mail.ru>
2023-12-01 19:31:37 +1000

    Считывание имени с клавиатуры и выводим на экран

---
+++ Lecture_1/Example002_UserHello/InputName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_1.Example002_UserHello
+{
+    class InputName
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя: ");
+            string userName = Console.ReadLine();
+            Console.Write("Привет, ");
+            Console.Write(userName);
+            Console.ReadLine();
+        }
+    }
+}
