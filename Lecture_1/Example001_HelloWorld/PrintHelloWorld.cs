c6c26e9060b22da0df7332e60f0bef38c882efcb
Алескей Щербаков <manay.ru@mail.ru>
2023-12-01 19:23:01 +1000

    Моя первая программа(вывод на экран Hello World)

---
+++ Lecture_1/Example001_HelloWorld/PrintHelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_1.Example001_HelloWorld
+{
+    class PrintHelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
