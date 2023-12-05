c0ccd09970c051e8e251e4de86da038525ac0142
Алескей Щербаков <manay.ru@mail.ru>
2023-12-05 19:13:06 +1000

    Массив - поиск по индексу

---
+++ Lecture_2/Example003_MethodArray/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_2.Example003_MethodArray
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 13, 28, 57, 13, 34, 56, 46, 57, 28, 91 };
+
+            int n = array.Length;
+
+            int find = 57;
+            int find1 = 34;
+            int index = 0;
+            while(index < n)
+            {
+                if(array[index] == find)
+                {
+                    Console.WriteLine($"{index}");
+                    break;
+                }              
+                index++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
